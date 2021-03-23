84d7d0694a75781649319bee2324d20438d5bf49
Saied Hassaninia <shassaninia@simpluris.com>
2021-03-23 00:42:53 -0400

    form link darker blue

--- components/FormItem.tsx
+++ components/FormItem.tsx
@@ -2,17 +2,17 @@ import React from 'react'
 import Link from 'next/link';
 import { IForm } from '../types'
 
-function FormItem({form}:{form:IForm}) {
-    return (
-        <li className="flex items-center hover:bg-gray-300">
-              <Link as={`/form/${form.WebFormId}`} href={'/form/[webFormId]'}>
-              <a className="text-blue-600">
-                {form.WebsiteLinkTitle}
-              </a>
-              </Link>
-              
-            </li>
-    )
+function FormItem({ form }: { form: IForm }) {
+  return (
+    <li className="flex items-center hover:bg-gray-300">
+      <Link as={`/form/${form.WebFormId}`} href={'/form/[webFormId]'}>
+        <a className="text-blue-700">
+          {form.WebsiteLinkTitle}
+        </a>
+      </Link>
+
+    </li>
+  )
 }
 
 export default FormItem
